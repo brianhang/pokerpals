1b9a11c0e83fffed245e05294a1ba42661d2b45b
Brian Hang <hangwithbrian@gmail.com>
2024-04-28 16:41:30 -0700

    Fix VS Code test discovery

--- src/tests/test_payout_settle.py
+++ src/tests/test_payout_settle.py
@@ -3,14 +3,15 @@ from typing import Optional
 import unittest
 
 from game_players.game_players import GamePlayer
-from .settle import get_transactions, Transaction
+from payout.settle import get_transactions, Transaction
 
 
 class TestSettle(unittest.TestCase):
     def test_empty(self):
         game_players = self.make_players([])
         transactions = get_transactions(game_players)
-        self.assertListEqual([], transactions, 'There should be no transactions with no players')
+        self.assertListEqual(
+            [], transactions, 'There should be no transactions with no players')
 
     def test_no_op(self):
         game_players = self.make_players([(0, 0)])
@@ -23,7 +24,8 @@ class TestSettle(unittest.TestCase):
         self.assertListEqual([], transactions, 'No transactions are necessary')
 
     def test_no_op_nonzero_many(self):
-        game_players = self.make_players([(10_00, 10_00), (20_00, 20_00), (0, 0)])
+        game_players = self.make_players(
+            [(10_00, 10_00), (20_00, 20_00), (0, 0)])
         transactions = get_transactions(game_players)
         self.assertListEqual([], transactions, 'No transactions are necessary')
 
