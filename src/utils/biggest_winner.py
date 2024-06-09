f02a6236c82686fc802fe9b4e3160d7f042f1335
Brian Hang <hangwithbrian@gmail.com>
2024-06-09 00:47:13 -0700

    Ignore spectators for biggest winner

--- src/utils/biggest_winner.py
+++ src/utils/biggest_winner.py
@@ -1,3 +1,4 @@
+import math
 from typing import Optional
 
 from game_players.game_players import GamePlayer
@@ -7,4 +8,14 @@ def find_biggest_winner(game_players: list[GamePlayer]) -> Optional[GamePlayer]:
     if not game_players:
         return None
 
-    return max(game_players, key=lambda player: player.earnings_cents())
+    highest_earnings = -math.inf
+    biggest_winner = None
+
+    for player in game_players:
+        earnings = player.earnings_cents()
+
+        if earnings and earnings > highest_earnings:
+            biggest_winner = player
+            highest_earnings = earnings
+
+    return biggest_winner
